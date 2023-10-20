39a331f6809b68a69ab9268b639153f4ac7c6c29
aryanmandya <aryanmandya4@gmail.com>
2023-10-20 22:02:02 +0530

    changed frontend

---
+++ client/src/components/Circle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Circle.css'
+import {FaRobot} from 'react-icons/fa'
+
+function Circle() {
+  return (
+    <div class='circle-container'>
+      <FaRobot/>
+    </div>
+  )
+}
+
+export default Circle

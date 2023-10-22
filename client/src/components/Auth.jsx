32d872907982092070b21fec931d0a133ddf4335
aryanmandya <aryanmandya4@gmail.com>
2023-10-22 13:22:37 +0530

    Fixed a bug

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 
-import signinImage from '../assets/signup.jpg';
 
 const cookies = new Cookies();
 
@@ -28,8 +27,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
-        // const URL = 'https://medical-pager.herokuapp.com/auth';
+        const URL = 'https://chat-app-server-vike.onrender.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
